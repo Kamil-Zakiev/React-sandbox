83a2ef76cfc0fd9de77dfd16c90be28318b899d3
Kamil Zakiev <kamil.m.zakiev@akvelon.com>
2019-06-07 23:11:05 +0300

    added X/O interleaving

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ class Board extends React.Component {
     super(props);
 
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
 
@@ -28,14 +29,17 @@ class Board extends React.Component {
     );
   }
 
-  handleClick(i){
+  handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({ 
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
