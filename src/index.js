fdb15a22f46cd4dcf8e7f1b786350309f5162175
Kamil Zakiev <kamil.m.zakiev@akvelon.com>
2019-06-07 23:25:51 +0300

    added game logic

--- src/index.js
+++ src/index.js
@@ -30,6 +30,10 @@ class Board extends React.Component {
   }
 
   handleClick(i) {
+    if(calculateWinner(this.state.squares) || this.state.squares[i]){
+      return;
+    }
+
     const squares = this.state.squares.slice();
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({ 
@@ -39,7 +43,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = winner + ' has won!';
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -86,4 +96,25 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+// helper function
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
   
