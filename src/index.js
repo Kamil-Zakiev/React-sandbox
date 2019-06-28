88e4c09d9a03cba494e23d5cfed46a51d984c2f0
Kamil Zakiev <kamil.m.zakiev@akvelon.com>
2019-06-28 19:05:58 +0300

    added saga for step timeout

--- src/index.js
+++ src/index.js
@@ -7,17 +7,25 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './store/reducer'
 import createSagaMiddleware from 'redux-saga';
-import cellClickingSaga from './store/cellClickingSaga';
+import { all } from 'redux-saga/effects'
+
+import { cellClickingSaga, opponentHasPressed } from './store/cellClickingSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__;
 const store = createStore(reducer,
     compose(
         applyMiddleware(sagaMiddleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        ));
-    
-sagaMiddleware.run(cellClickingSaga);
+        reduxDevTools && reduxDevTools()
+    ));
+
+sagaMiddleware.run(function* rootSaga() {
+    yield all([
+        cellClickingSaga(),
+        opponentHasPressed()
+    ]);
+});
 
 ReactDOM.render(
     <Provider store={store}>
--- src/store/cellClickingSaga.js
+++ src/store/cellClickingSaga.js
@@ -1,4 +1,4 @@
-import { call, put, takeEvery } from 'redux-saga/effects'
+import { call, put, takeEvery, race, take, delay, select } from 'redux-saga/effects'
 import { CLICK_CELL } from './actions';
 
 function* clickCell(action) {
@@ -6,7 +6,25 @@ function* clickCell(action) {
     yield put({ type: "CELL_PRESSED" });
 }
 
-export default function* cellClickingSaga() {
+export function* opponentHasPressed() {
+    let opponentClick = yield take(CLICK_CELL);
+    while (true) {
+        const { clicked } = yield race({
+            clicked: take(CLICK_CELL),
+            timeout: delay(5000)
+        });
+
+        if (!clicked) {
+            const opponent = yield select(s => s.history[s.stepNumber].squares[opponentClick.cell]);
+            console.log('timeout loose, ', opponent, ' has won!');
+            break;
+        } else {
+            opponentClick = clicked;
+        }
+    }
+}
+
+export function* cellClickingSaga() {
     yield takeEvery(CLICK_CELL, clickCell);
 }
 
@@ -16,5 +34,5 @@ function apiSendRequest(action) {
             console.log('clicked, ', action);
             resolve();
         }, 500);
-    });  
+    });
 }
